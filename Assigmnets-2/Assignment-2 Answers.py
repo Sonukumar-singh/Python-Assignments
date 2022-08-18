ebd10a2facf6189754420f40ad830c3b209ef81f
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-19 00:07:20 +0530

    Add files via upload

---
+++ Assigmnets-2/Assignment-2 Answers.py
@@ -0,0 +1,84 @@
+# <---- Answer 1 ---->
+# This is a comment line
+# print("Learning Python")
+
+
+# <---- Answer 2 ---->
+
+"""
+There are four variable in python
+a = "sonu kumar" ---> Type of Variable is str
+b = 12           ---> Type of Variable is int
+c = True/False   ---> Type of Variable is bool
+d = 62.47        ---> Type of Variable is float
+"""
+
+
+# <---- Answer 3 ---->
+# a = "sonu-kumar"
+# b = 12
+# c = True
+# d = 62.47
+# e = 5+1j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+
+# <---- Answer 4 ---->
+# x = 5
+# y = 5
+# print(id(x))
+# print(id(y))
+
+
+# <---- Answer 5 ---->
+# a = "sonu kumar"
+# print(type(a))
+# print(id(a))
+
+# b = 25
+# print(type(b))
+# print(id(b))
+
+# c = 45.25
+# print(type(c))
+# print(id(c))
+
+# d = True
+# print(type(d))
+# print(id(d))
+
+
+# <---- Answer 6 ---->
+# import keyword
+# print(keyword.kwlist)
+
+
+# <---- Answer 7 ---->
+"""
+    Step 1 = Open Idle shell 
+    Step 2 = type help()
+    Step 3 = then type keywords
+
+   (You will get list of all keyword)
+   """
+
+# <---- Answer 8 ---->
+#  see second and third file A0.py and A1.py
+
+
+# <---- Answer 9 ---->
+# True False None
+
+
+# <---- Answer 10 ---->
+# date, month, year = 13, 8, 2022
+# print(date, month, year, sep="-")
+
+# hour, sec = 9, 00
+# d = "PM"
+# print(hour, sec, sep=":", end=" ")
+# print(d)
