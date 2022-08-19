b7a13b3640b78c91bbf2045b39a29cad6ed5f2c4
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-19 10:52:57 +0530

    Add files via upload

---
+++ Assigmnets-3/Assignment-3 Answers.py
@@ -0,0 +1,64 @@
+# <---- Answer 1 ---->
+a = 5
+print(a, type(a))
+
+
+a = str(5)
+print(a, type(a))
+
+
+# <---- Answer 2 ---->
+x = "m"
+print(ord(x))
+
+
+# <---- Answer 3 ---->
+x = 100
+print(chr(x))
+
+
+# <---- Answer 4 ---->
+y = 24
+print("This is a Number :  ", y)
+print("And this is binary :  ", bin(y))
+
+
+# <---- Answer 5 ---->
+d = 20
+print("This is a Number :  ", d)
+print("And this is octal :  ", oct(d))
+
+
+# <---- Answer 6 ---->
+e = 32
+print("This is a Number :  ", e)
+print("And this is hexadecimal :  ", hex(e))
+
+
+# <---- Answer 7 ---->
+x = 0b1100101
+print(x)
+
+
+# <---- Answer 8 ---->
+x = 0x2F
+print(oct(x))
+
+
+# <---- Answer 9 ---->
+x = 0o125
+print(bin(x))
+
+
+# <---- Answer 10 ---->
+a = 25
+b = 39
+print(oct(a))
+print(hex(b))
+c = a+b
+print(c, bin(c))
+
+d = 0o31
+e = 0x27
+f = d+e
+print(bin(f))
