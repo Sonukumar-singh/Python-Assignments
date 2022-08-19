7579d4046b6053f7a80eca44f817f54eaa5f8709
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-19 17:21:21 +0530

    Add files via upload

---
+++ Assigmnets-4/Assignment-4 Answers.py
@@ -0,0 +1,67 @@
+# <---- Answer 1 ---->
+x = input("Enter Full Name: ")
+print(x)
+
+
+# <---- Answer 2 ---->
+x = int(input("Enter Number: "))
+print(type(x), x)
+
+
+# <---- Answer 3 ---->
+x = int(input("Enter First Number: "))
+y = int(input("Enter second Number: "))
+z = x+y
+print(z)
+
+
+# <---- Answer 4 ---->
+r = int(input("Enter Radius : "))
+a = (22/7*(r**2))
+print("Area of Circle is : ", a)
+
+
+# <---- Answer 5 ---->
+n = int(input("Enter a Number : "))
+s = (n**2)
+print("Sqare of a Number is : ", s)
+
+
+# <---- Answer 6 ---->
+b, h = int(input("Enter Base Of Triangle: ")), int(
+    input("Enter Height of Traingle: "))
+
+a = (1/2*(b*h))
+print("Area of Traingle is : ", a)
+
+
+# <---- Answer 7 ---->
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+
+print("Average = ", ((a+b+c) / 3))
+
+
+# <---- Answer 8 ---->
+p = 8
+t = 6
+r = 8
+
+si = (p*r*t)/100
+print("Simple Interest is: ", si)
+
+
+# <---- Answer 9 ---->
+l = 10
+b = 25
+h = 20
+vc = l*b*h
+print("Volume of Cuboid is: ", vc)
+
+
+# <---- Answer 10 ---->
+l = 20
+w = 10
+a = w*l
+print("Area of Rectangle is: ", a)
