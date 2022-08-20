e253c8dd9735d8d639d7fd9b3caf833ee8b4525f
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-20 20:00:01 +0530

    Add files via upload

---
+++ Assigmnets-1/Assignment-1 Answers.py
@@ -0,0 +1,50 @@
+# <---- Answer 1 ---->
+"""
+Step 1 : Open CMD on Computer using ( window +r, and type cmd on run option )
+Step 2 : type (python -V) version will be display on cmd option.
+
+"""
+
+
+# <---- Answer 2 ---->
+# In cmd option type python then type print
+print("Hello python")
+
+
+# <---- Answer 3 ---->
+
+
+# <---- Answer 4 ---->
+print("Hello python")
+
+
+# <---- Answer 5 ---->
+print("Hello", sep="\n")
+print("python")
+
+
+# <---- Answer 6 ---->
+name = "MySirG"
+print(name)
+
+
+# <---- Answer 7 ---->
+name = "Teacher's Day"
+print(name)
+
+
+# <---- Answer 8 ---->
+Fname = "sonu kumar"
+print(type(Fname))
+
+
+# <---- Answer 9 ---->
+fname, age, qualification, experience = "sonu kumar", 23, "Bcom", 0
+print(fname, age, qualification, experience)
+
+
+# <---- Answer 10 ---->
+variable1 = 55
+variable2 = 30
+variable3 = 5
+print(variable1, variable2, variable3, sep="\n")
