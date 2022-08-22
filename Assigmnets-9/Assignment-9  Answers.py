6f7d291ede697ce34f170aec45fedc25066fc998
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-22 21:32:04 +0530

    Add files via upload

---
+++ Assigmnets-9/Assignment-9  Answers.py
@@ -0,0 +1,70 @@
+# <---- Answer 1 ---->
+a = 1
+while (a >= 1):
+    print("MySirG", end=' ')
+    a = a+1
+
+
+# <---- Answer 2 ---->
+a = 1
+while (a >= 1):
+    print(a, end=' ')
+    a = a+1
+
+
+# <---- Answer 3 ---->
+a = 1
+while (a <= 1):
+    print(a, end=' ')
+    a = a-1
+
+
+# <---- Answer 4 ---->
+a = 1
+while (a >= 1):
+    if (a % 2 == 1):
+        print(a, end=' ')
+    a = a+1
+
+
+# <---- Answer 5 ---->
+a = -1
+while (a <= -1):
+    if (a % 2 == 1):
+        print(a, end=' ')
+    a = a-1
+
+
+# <---- Answer 6 ---->
+a = 1
+while (a >= 1):
+    if (a % 2 == 0):
+        print(a, end=' ')
+    a = a+1
+
+
+# <---- Answer 7 ---->
+a = 1
+while (a <= 1):
+    if (a % 2 == 0):
+        print(a, end=' ')
+    a = a-1
+
+
+# <---- Answer 8 ---->
+a = 1
+while (a >= 1):
+    print(a**2, end=' ')
+    a = a+1
+
+# <---- Answer 9 ---->
+a = 1
+while (a >= 1):
+    print(a**3, end=' ')
+    a = a+1
+
+# <---- Answer 10 ---->
+a = 1
+while (a >= 1):
+    print(a*a, end=' ')
+    a = a+1
