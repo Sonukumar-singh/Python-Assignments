481346f9e707812a351df654670de018b1cfb28f
Sonu Kumar <84267922+Sonukumar-singh@users.noreply.github.com>
2022-08-26 20:21:53 +0530

    Add files via upload

---
+++ Assigmnets-5/Assignment-5  Answers.py
@@ -0,0 +1,53 @@
+# <---- Answer 1 ---->
+a = 2534
+print(a//10)
+
+
+# <---- Answer 2 ---->
+a = 2089
+print(a % 10)
+
+
+# <---- Answer 3 ---->
+a, b = 10, "sonu"
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+
+# <---- Answer 4 ---->
+x = int(input("Enter First Number: "))
+y = int(input("Enter second Number: "))
+print(x**y)
+
+
+# <---- Answer 5 ---->
+num = input("Enter three digit number : ")
+print("First Digit Number is : ", num[0])
+
+
+# <---- Answer 6 ---->
+num = input("Enter three digit number : ")
+print("Middle Digit Number is : ", num[1])
+
+
+# <---- Answer 7 ---->
+num = input("Enter three digit number : ")
+print("last Digit Number is : ", num[2])
+
+
+# <---- Answer 8 ---->
+x = [4, 15, 8, 12, 11]
+print(17 in x)
+
+
+# <---- Answer 9 ---->
+x = [4, 15, 8, 12, 11]
+print(17 not in x)
+
+
+# <---- Answer 10 ---->
+x = 25
+y = 25
+print(x is y)
